6a18ea864aeb789ac30e0becc0883aab49d0e7ea
Marcelo Amuchastegui <subizbrasil@gmail.com>
2021-01-07 17:41:51 -0300

    alteração filtro de preços

--- src/Componentes/FiltroPrecos/FiltroPrecos.jsx
+++ src/Componentes/FiltroPrecos/FiltroPrecos.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
+/*
+const precos = [189, 348, 390, 673, 998,]
+
+function App() {
+    return (
+        <div>
+            <FiltroPrecos lista={precos} min={300} max={1000} />
+        </div>
+    )
+}*/
+
 export default function FiltroPrecos(props) {
     const ListaFiltrada = props.lista.filter((preco) => {
         if (preco > props.min) {
@@ -7,14 +18,21 @@ export default function FiltroPrecos(props) {
         } else {
             return false
         }
+    }).filter((preco) => {
+        if (preco > props.max) {
+            return true
+        } else {
+            return false
+        }
     })
+
     console.log(ListaFiltrada);
 
     return (
         <div>
             <h2>FiltroPreços</h2>
             <ul>
-                {props.lista.map(preco => {
+                {ListaFiltrada.map(preco => {
                     return <li>{preco}</li>
                 })}
             </ul>
